887ccd9772b9dffa0228ad90615c1ab35e41ccdd
shado <shadowstakeus28@gmail.com>
2023-07-25 22:35:06 +0300

    feat: constructHtmlField

--- purejs.js
+++ purejs.js
@@ -1,2 +1,18 @@
+// const container = document.querySelector('.container')
+
+const FIELD_SIZE = 5;
+
+function constructHtmlField(FIELD_SIZE){
+    let html = ""
+    for (let index = 0; index < FIELD_SIZE; index++) {
+        html += '<ul>'
+        for (let index = 0; index < FIELD_SIZE; index++) {        
+            html += "<li></li>"
+        }
+        html += "</ul>"
+    }
+    return html
+}
+
 
 // 
