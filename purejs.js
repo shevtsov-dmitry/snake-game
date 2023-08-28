4cf362999bc43b79300280900bb6dd2e7954ff8f
shado <shadowstakeus28@gmail.com>
2023-08-28 18:11:33 +0300

    feat: remove scaling of tail. now size always the same until snake will eat apple

--- purejs.js
+++ purejs.js
@@ -26,6 +26,7 @@ let x = y
 let snakeHead = rows[y].children[x]
 
 snakeHead.classList.add('snake-head')
+
 // movement
 let movingRight = false, movingLeft = false, movingUp = false , movingDown = false
 document.addEventListener('keydown', (event) => {
@@ -50,14 +51,28 @@ document.addEventListener('keydown', (event) => {
 })
 
 setInterval(() => {
-    if(movingUp) y--
-    if(movingDown) y++
-    if(movingLeft) x--
-    if(movingRight) x++
+    if(movingUp) {
+        y--
+        rows[y + 1].children[x].classList.remove('snake-head');
+    }
+    if(movingDown){
+        y++
+        rows[y - 1].children[x].classList.remove('snake-head');
+    }
+    if(movingLeft) {
+        x--
+        rows[y].children[x + 1].classList.remove('snake-head');
+    }
+    if(movingRight) {
+        x++
+        rows[y].children[x - 1].classList.remove('snake-head');
+    }
 
     rows[y].children[x].classList.add('snake-head')
 
     // rows[y].children[x].classList.add('snake-head')
     // rows[y - 1].children[x - 1].classList.remove('snake-head')
 }, 500)
+
+// spawn apple
 
