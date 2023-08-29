c28f1f032473a33e7f3405c3da1a28254bde7a89
shado <shadowstakeus28@gmail.com>
2023-08-29 13:01:23 +0300

    feat: randomly spawn apples on map and snake increases size when eats an apple

--- purejs.js
+++ purejs.js
@@ -1,13 +1,13 @@
 const field = document.querySelector('.container')
 
-const FIELD_SIZE = 20
+const FIELD_SIZE = 10
 const CELL_SIZE = 30
 
-function constructHtmlField(FIELD_SIZE){
+function constructHtmlField(FIELD_SIZE) {
     let html = ""
     for (let index = 0; index < FIELD_SIZE; index++) {
         html += '<ul>'
-        for (let index = 0; index < FIELD_SIZE; index++) {        
+        for (let index = 0; index < FIELD_SIZE; index++) {
             html += "<li></li>"
         }
         html += "</ul>"
@@ -24,55 +24,69 @@ let rows = field.children
 let y = FIELD_SIZE / 2
 let x = y
 let snakeHead = rows[y].children[x]
+let apple = randomizeApplePosition()
+
+function randomizeApplePosition(){
+    let apl = rows[FIELD_SIZE - Math.floor(Math.random() * FIELD_SIZE)]
+        .children[FIELD_SIZE - Math.floor(Math.random() * FIELD_SIZE)];
+    if(apl.classList.contains('snake-head')) randomizeApplePosition()
+    apl.classList.add('apple')
+    return apl
+}
 
 snakeHead.classList.add('snake-head')
-
 // movement
-let movingRight = false, movingLeft = false, movingUp = false , movingDown = false
+let movingRight = false, movingLeft = false, movingUp = false, movingDown = false
 document.addEventListener('keydown', (event) => {
-    switch (event.key){
+    switch (event.key) {
         case "ArrowUp":
-            movingUp = true, movingLeft = false, 
-            movingDown = false, movingRight = false
-        break
+            movingUp = true, movingLeft = false,
+                movingDown = false, movingRight = false
+            break
         case "ArrowDown":
-            movingUp = false, movingLeft = false, 
-            movingDown = true, movingRight = false
-        break
+            movingUp = false, movingLeft = false,
+                movingDown = true, movingRight = false
+            break
         case "ArrowLeft":
-            movingUp = false, movingLeft = true, 
-            movingDown = false, movingRight = false
-        break
-        case "ArrowRight": 
-            movingUp = false, movingLeft = false, 
-            movingDown = false, movingRight = true
-        break
+            movingUp = false, movingLeft = true,
+                movingDown = false, movingRight = false
+            break
+        case "ArrowRight":
+            movingUp = false, movingLeft = false,
+                movingDown = false, movingRight = true
+            break
     }
 })
 
 
 let snakeHeadChildren = []
-
 console.log(snakeHeadChildren)
-setInterval(() => {
-    if(movingUp) {
+
+function changeXandYonMovement() {
+    // moving inputs action to change X and Y
+    if (movingUp) {
         y--
         // rows[y + 1].children[x].classList.remove('snake-head');
     }
-    if(movingDown){
+    if (movingDown) {
         y++
         // rows[y - 1].children[x].classList.remove('snake-head');
     }
-    if(movingLeft) {
+    if (movingLeft) {
         x--
         // rows[y].children[x + 1].classList.remove('snake-head');
     }
-    if(movingRight) {
+    if (movingRight) {
         x++
         // rows[y].children[x - 1].classList.remove('snake-head');
     }
+}
 
-    if(!snakeHeadChildren.includes(rows[y].children[x]) )
+setInterval(() => {
+    changeXandYonMovement();
+
+    // If statement makes one field sell not to add itself to @snakeHeadChildren continuously
+    if (!snakeHeadChildren.includes(rows[y].children[x]))
         snakeHeadChildren.push(rows[y].children[x])
 
     for (const snakeHeadChild of snakeHeadChildren) {
@@ -84,19 +98,24 @@ setInterval(() => {
     for (let i = 0; i < FIELD_SIZE; i++) {
         for (let j = 0; j < FIELD_SIZE; j++) {
             const element = rows[i].children[j]
-            if(element.classList.contains('snake-head')
-                && !snakeHeadChildren.includes(element) ) element.classList.remove('snake-head')
+            if (element.classList.contains('snake-head')
+                && !snakeHeadChildren.includes(element)) element.classList.remove('snake-head')
+
+            // make snake longer when it eats an apple
+            if (element.classList.contains('snake-head') && element.classList.contains('apple')) {
+                element.classList.remove('apple')
+                apple = randomizeApplePosition()
+                snakeHeadChildren.push(rows[y].children[x])
+
+            }
         }
     }
-    if(snakeHeadChildren.length != 1) snakeHeadChildren.shift()
+    if (snakeHeadChildren.length != 1) snakeHeadChildren.shift()
+
+    apple.classList.add('apple')
+    // console.log("apple: " + apple);
+
+
 }, 500)
 
-// spawn apple
 
-
-function spawnApple(){
-    let apple = rows[y + 2].children[x + 2]
-    apple.classList.add('.apple')
-}
-
-spawnApple()
