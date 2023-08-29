ed253d3d43795518c9da08ab561fc463d6f78b94
shado <shadowstakeus28@gmail.com>
2023-08-29 11:14:03 +0300

    feat: make snake moving in field with its body parts following her. WARNING! bad optimization related to scanning whole game field to remove previous body parts.

--- purejs.js
+++ purejs.js
@@ -50,29 +50,53 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
+
+let snakeHeadChildren = []
+
+console.log(snakeHeadChildren)
 setInterval(() => {
     if(movingUp) {
         y--
-        rows[y + 1].children[x].classList.remove('snake-head');
+        // rows[y + 1].children[x].classList.remove('snake-head');
     }
     if(movingDown){
         y++
-        rows[y - 1].children[x].classList.remove('snake-head');
+        // rows[y - 1].children[x].classList.remove('snake-head');
     }
     if(movingLeft) {
         x--
-        rows[y].children[x + 1].classList.remove('snake-head');
+        // rows[y].children[x + 1].classList.remove('snake-head');
     }
     if(movingRight) {
         x++
-        rows[y].children[x - 1].classList.remove('snake-head');
+        // rows[y].children[x - 1].classList.remove('snake-head');
     }
 
-    rows[y].children[x].classList.add('snake-head')
+    if(!snakeHeadChildren.includes(rows[y].children[x]) )
+        snakeHeadChildren.push(rows[y].children[x])
 
-    // rows[y].children[x].classList.add('snake-head')
-    // rows[y - 1].children[x - 1].classList.remove('snake-head')
+    for (const snakeHeadChild of snakeHeadChildren) {
+        snakeHeadChild.classList.add('snake-head')
+    }
+
+    console.log(snakeHeadChildren);
+
+    for (let i = 0; i < FIELD_SIZE; i++) {
+        for (let j = 0; j < FIELD_SIZE; j++) {
+            const element = rows[i].children[j]
+            if(element.classList.contains('snake-head')
+                && !snakeHeadChildren.includes(element) ) element.classList.remove('snake-head')
+        }
+    }
+    if(snakeHeadChildren.length != 1) snakeHeadChildren.shift()
 }, 500)
 
 // spawn apple
 
+
+function spawnApple(){
+    let apple = rows[y + 2].children[x + 2]
+    apple.classList.add('.apple')
+}
+
+spawnApple()
